69006e1e3cac023c69689cef910c53983497bfb4
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-14 09:35:23 +0100

    enunciats

---
+++ nivell1/src/n1exercici1/App.java
@@ -0,0 +1,8 @@
+package n1exercici1;
+/*
+Exercici 1. Crea una jerarquia d'objectes Java on els objectes fill sobreescriguin alguna funcionalitat
+de l'objecte pare emprant @Override. Afegeix una classe que efectuï les invocacions necessàries
+demostrant el punt anterior.
+ */
+public class App {
+}
---
+++ nivell1/src/n1exercici2/App.java
@@ -0,0 +1,8 @@
+package n1exercici2;
+/*
+Exercici 2. Afegeix a les classes fills alguns mètodes deprecados, i utilitzi la anotacion corresponent.
+Invoqui des d'una classe externa els mètodes deprecados, suprimint mitjançant l'anotació corresponent
+els warnings per ser deprecada la seva utilització.
+ */
+public class App {
+}
